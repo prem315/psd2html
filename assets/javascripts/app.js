e96312167ee6c70ff5df0c4c124a4d673a12f531
Prem Patel <prem.patel3105@gmail.com>
2015-09-17 18:48:57 +0530

    Final Commit

---
+++ assets/javascripts/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('header nav').affix({
+  		offset: {
+    		top: 60,
+  		}
+	});
+
+	$('body').scrollspy({ 
+		target: '#nav-link-container',
+		offset: 55 
+	});
+
+	var email_components = ["hello", "@", "example", ".", "com"]
+	var email = email_components.join("");
+	$('#email-link').html(email).attr('href', 'mailto:'+email);
+});
